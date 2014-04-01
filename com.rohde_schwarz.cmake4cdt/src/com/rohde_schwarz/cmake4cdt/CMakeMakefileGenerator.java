83dbecb88252db09a46c717c25049502e98a44be
rungemar <martin.runge@rohde-schwarz.com>
2014-04-01 18:18:13 +0200

    about to call cmake

--- com.rohde_schwarz.cmake4cdt/src/com/rohde_schwarz/cmake4cdt/CMakeMakefileGenerator.java
+++ com.rohde_schwarz.cmake4cdt/src/com/rohde_schwarz/cmake4cdt/CMakeMakefileGenerator.java
@@ -1,5 +1,14 @@
 package com.rohde_schwarz.cmake4cdt;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
 import org.eclipse.cdt.managedbuilder.makegen.IManagedBuilderMakefileGenerator;
 import org.eclipse.core.resources.IProject;
@@ -9,12 +18,21 @@ import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.variables.IStringVariableManager;
 import org.eclipse.core.variables.IValueVariable;
 import org.eclipse.core.variables.VariablesPlugin;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 import com.rohde_schwarz.cmake4cdt.Activator;
 import com.rohde_schwarz.cmake4cdt.ui.CMakePropertyConstants;
@@ -26,6 +44,8 @@ public class CMakeMakefileGenerator implements IManagedBuilderMakefileGenerator 
 	String projectName;
 	IManagedBuildInfo buildInfo;
 	IProject project;
+	
+	public final String CMAKE_EXE = "cmake";
 
 	@Override
 	public void generateDependencies() throws CoreException {
@@ -99,6 +119,7 @@ public class CMakeMakefileGenerator implements IManagedBuilderMakefileGenerator 
 
 	@Override
 	public String getMakefileName() {
+		// @TODO get settings for selected CMake generator and adjust Makefile Name (e.g. in case of ninja make)
 		return "Makefile";
 	}
 
@@ -122,8 +143,156 @@ public class CMakeMakefileGenerator implements IManagedBuilderMakefileGenerator 
 
 	@Override
 	public MultiStatus regenerateMakefiles() throws CoreException {
-		// TODO Auto-generated method stub
-		return null;
+		MultiStatus mstatus = new MultiStatus("com.rohde_schwarz.cmake4cdt.builder", 0, "success", null );
+
+		MessageConsole buildifConsole = findConsole("CMake");
+		MessageConsoleStream out = buildifConsole.newMessageStream();
+
+		IStringVariableManager varMgr = VariablesPlugin.getDefault().getStringVariableManager();
+
+//		IValueVariable buildifProjectDirVar = varMgr.getValueVariable("BuildIF_ProjectPath");
+//		buildifProjectDirVar.setValue(projDir.toString());
+		
+		// IConfiguration activeConfig = buildInfo.getDefaultConfiguration();
+		String currentConf = buildInfo.getConfigurationName();
+		String currentArch = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_CURRENT_TARGET_ARCH);
+		String currentInstrument = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_CURRENT_TARGET_DEVICE);
+		
+		String buildDir = null;
+		
+		IEclipsePreferences projectProperties = new ProjectScope(project).getNode("com.rohde_schwarz.cmake4cdt.scope");
+		boolean instSpecific = false;
+		boolean useWorkspaceBuidDirSettings = true;
+		if (projectProperties != null) {
+			instSpecific = projectProperties.getBoolean(CMakePropertyConstants.P_IS_INSTRUMENT_SPECIFIC, false);
+			useWorkspaceBuidDirSettings = projectProperties.getBoolean(CMakePropertyConstants.P_USE_WORKSPACE_BUILDDIR_SETTINGS, true);
+
+			buildDir = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_BUILDDIR);
+
+			if(!useWorkspaceBuidDirSettings) {
+				buildDir = projectProperties.get( CMakePropertyConstants.P_BUILD_PATH, buildDir);
+			}
+			
+		}
+
+		IValueVariable destdirVar = varMgr.getValueVariable("CMake_Destdir");
+		String destDirStr = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_DESTDIR);
+		destDirStr = varMgr.performStringSubstitution(destDirStr);
+		destdirVar.setValue( destDirStr );
+
+		
+		buildDir = varMgr.performStringSubstitution(buildDir);
+		List<String> cmakeCmd = new ArrayList<String>();
+		
+		cmakeCmd.add( CMAKE_EXE );
+
+		cmakeCmd.add("-DCMAKE_BUILD_TYPE=");
+		cmakeCmd.add("-DCMAKE_TOOLCHAIN_FILE=");
+		
+		cmakeCmd.add("-DCMAKE_EXPORT_COMPILE_COMMANDS=on");
+
+		// add cmake extra FLAGS
+		
+		// add some cmake options
+		
+		if(instSpecific &&  currentInstrument != PreferenceConstants.P_NOT_INST_SPECIFIC ) {
+			cmakeCmd.add("-i " + currentInstrument);
+		}
+		
+		if(buildDir != null) {
+			cmakeCmd.add("-g " + buildDir);
+		}
+		// String name = project.getName();
+
+		String message = new String("invoking: ");
+		for(int i=0; i < cmakeCmd.size(); i++)
+		{
+			message += cmakeCmd.get(i);
+		}
+		// out.println(message);
+		
+		ProcessBuilder pb = new ProcessBuilder(cmakeCmd);
+		// ProcessBuilder pb = new ProcessBuilder("env");
+		IPath wdir = project.getLocation();
+		pb.directory(wdir.toFile());
+		pb.redirectErrorStream(true);
+		
+		message = "working dir: " + wdir;
+		out.println( message );
+		
+//		Map<String,String> env = pb.environment();
+//		env.put("INSTRUMENT", currentInstrument);
+
+		try { 
+			Process proc = pb.start();
+			InputStream instream = proc.getInputStream();
+			
+			proc.waitFor();
+			
+			String buildifOutput = inputStream2String(instream);
+			out.print(buildifOutput);
+
+			Status status = new Status( org.eclipse.core.runtime.IStatus.OK, "com.rohde_schwarz.cmake4cdt", 0, "success", null);
+			mstatus.add(status);
+			
+		} catch (Exception ex) {
+			return reportError(ex.getMessage());
+		}
+		
+		return mstatus;
 	}
+	
+	 private MessageConsole findConsole(String name) {
+	      ConsolePlugin plugin = ConsolePlugin.getDefault();
+	      IConsoleManager conMan = plugin.getConsoleManager();
+	      IConsole[] existing = conMan.getConsoles();
+	      for (int i = 0; i < existing.length; i++)
+	         if (name.equals(existing[i].getName()))
+	            return (MessageConsole) existing[i];
+	      //no console found, so create a new one
+	      MessageConsole myConsole = new MessageConsole(name, null);
+	      conMan.addConsoles(new IConsole[]{myConsole});
+	      return myConsole;
+	   }
+	 
+	 private String inputStream2String(InputStream is) {
+		 /*
+		  * To convert the InputStream to String we use the BufferedReader.readLine()
+		  * method. We iterate until the BufferedReader return null which means
+		  * there's no more data to read. Each line will appended to a StringBuilder
+		  * and returned as String.
+		  */
+		 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		 StringBuilder sb = new StringBuilder();
+		  
+		 String line = null;
+		 try {
+			 while ((line = reader.readLine()) != null) {
+				 sb.append(line + "\n");
+			 }
+		 } catch (IOException e) {
+			 e.printStackTrace();
+		 } finally {
+			 try {
+				 is.close();
+			 } catch (IOException e) {
+				 e.printStackTrace();
+			 }
+		 }
+		 return sb.toString();
+	 }
+	 
+	 MultiStatus  reportError(String errorMsg) {
+		 MessageConsole buildifConsole = findConsole("CMake");
+		 MessageConsoleStream out = buildifConsole.newMessageStream();
+
+		 Color col = out.getColor();
+		 out.setColor(new Color(Display.getCurrent(), 255, 0, 0));
+		 out.println("Error: " + errorMsg);
+		 out.setColor(col);
+
+		 MultiStatus status = new MultiStatus("com.rohde_schwarz.cmake4cdt", IStatus.ERROR, "CMake failed to generate build files. See CMake console for details.", null);
+		 return status;
+	 }
 
 }

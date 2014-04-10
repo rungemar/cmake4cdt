e063e53c926b444be574d93a5c8ae86510f0fb21
rungemar <martin.runge@rohde-schwarz.com>
2014-04-10 15:20:53 +0200

    Added copyright file headers

--- com.rohde_schwarz.cmake4cdt/src/org/eclipse/cdt/cmake/ui/DestdirFieldEditor.java
+++ com.rohde_schwarz.cmake4cdt/src/org/eclipse/cdt/cmake/ui/DestdirFieldEditor.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2014 Rohde & Schwarz GmbH & Co. KG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Thierry Lach - thierry.lach@bbdodetroit.com - Fix for Bug 37155
+ *     Martin Runge - initial implementation of cmake support
  *******************************************************************************/
 
 package org.eclipse.cdt.cmake.ui;

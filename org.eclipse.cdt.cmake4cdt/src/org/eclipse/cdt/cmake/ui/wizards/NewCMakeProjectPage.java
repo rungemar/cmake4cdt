5d10760628a25b3fdbf2eda7d1d3ac50bb9b151d
Marc-Andre Laperle <malaperle@gmail.com>
2015-08-19 22:27:10 -0400

    Add some missing copyrights
    I put 2014, 2015 just to be extra safe.

--- org.eclipse.cdt.cmake4cdt/src/org/eclipse/cdt/cmake/ui/wizards/NewCMakeProjectPage.java
+++ org.eclipse.cdt.cmake4cdt/src/org/eclipse/cdt/cmake/ui/wizards/NewCMakeProjectPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Rohde & Schwarz GmbH & Co. KG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Martin Runge - initial implementation of cmake support
+ *******************************************************************************/
 package org.eclipse.cdt.cmake.ui.wizards;
 
 import org.eclipse.core.resources.IContainer;
